5ae88f0ef3f269293777c503ad02f1541b422143
Shahriat Hossain <somel90@gmail.com>
2021-02-21 08:49:23 +0600

    Spring REST: Rest Controller, Retrieve POJOs as JSON, @PathVariable, Global Exception Handling

---
+++ spring-rest-demo/src/main/java/com/mycode/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,12 @@
+package com.mycode.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.mycode.springdemo")
+public class DemoAppConfig {
+
+}
