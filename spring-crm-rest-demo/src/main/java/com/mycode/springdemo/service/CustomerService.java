3bf6e709eb77a845a49e327b8ce04c303893ebab
Shahriat Hossain <somel90@gmail.com>
2021-02-23 09:33:47 +0600

    Spring REST - CRUD Database CRM Real Time Project

---
+++ spring-crm-rest-demo/src/main/java/com/mycode/springdemo/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.mycode.springdemo.service;
+
+import java.util.List;
+
+import com.mycode.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
